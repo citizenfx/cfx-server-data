1062cca38b23991d888f2de72ab42eea284a0814
Joel Wurtz <jwurtz@jolicode.com>
2017-05-29 16:50:52 +0200

    Allow to pass posix signal to executable

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 fx_root=`dirname "$(readlink -f "$0")"`
 
-MONO_PATH=$fx_root/mono/ mono $fx_root/CitizenMP.Server.exe $*
+MONO_PATH=$fx_root/mono/ exec mono $fx_root/CitizenMP.Server.exe $*
