0ef934e703fbc3fb027632671b8a403d94954e36
Ryan Bucshon <busheezy@gmail.com>
2017-06-13 20:25:28 -0500

    Remove: clock stuff

--- resources/fivem/fivem_client.lua
+++ resources/fivem/fivem_client.lua
@@ -3,7 +3,5 @@ AddEventHandler('onClientMapStart', function()
 
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
-    SetClockTime(24, 0, 0)
-    PauseClock(true)
     Citizen.Trace("ocms fivem end\n")
 end)
