494820eddb1ae4c48f58097a822fe95ed4bb2a94
Ryan Bucshon <busheezy@gmail.com>
2017-06-13 20:25:51 -0500

    Remove: debug lines and extra space

--- resources/fivem/fivem_client.lua
+++ resources/fivem/fivem_client.lua
@@ -1,7 +1,4 @@
 AddEventHandler('onClientMapStart', function()
-	Citizen.Trace("ocms fivem\n")
-
     exports.spawnmanager:setAutoSpawn(true)
     exports.spawnmanager:forceRespawn()
-    Citizen.Trace("ocms fivem end\n")
 end)
