b050d98e60fc3112de83b2097dfc57180eab4146
moscovium <moscovium@fivem.net>
2017-06-21 17:37:24 +0200

    remove old CitizenMP.Server files from the tree to prevent confusion

--- bin/README.txt
+++
@@ -1,3 +0,0 @@
-CitizenMP.Server.exe in bin/ is no longer use.
-
-Please run the executable from the root directory.
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-fx_root=`dirname "$(readlink -f "$0")"`
-
-MONO_PATH=$fx_root/mono/ mono $fx_root/CitizenMP.Server.exe $*
