30a4f43022d888dd91600b63d725b3f05bbb7781
moscovium <moscovium@fivem.net>
2018-03-25 14:56:08 +0200

    mapmanager: fixes and some cleanup

--- resources/[managers]/mapmanager/__resource.lua
+++ resources/[managers]/mapmanager/__resource.lua
@@ -1,11 +1,11 @@
 client_scripts {
-    "mapmanager_client.lua",
-    "mapmanager_shared.lua"
+    "mapmanager_shared.lua",
+    "mapmanager_client.lua"
 }
 
 server_scripts {
-    "mapmanager_server.lua",
-    "mapmanager_shared.lua"
+    "mapmanager_shared.lua",
+    "mapmanager_server.lua"
 }
 
 resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
@@ -16,3 +16,4 @@ server_export "changeGameType"
 server_export "changeMap"
 server_export "doesMapSupportGameType"
 server_export "getMaps"
+server_export "roundEnded"
