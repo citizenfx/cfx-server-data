54f75bca86a9b95ac9a20f6199df44adf61a4cc6
blattersturm <peachypies@protonmail.ch>
2019-10-21 15:04:55 +0200

    runcode: improved UI, in-game support, JS support

--- resources/runcode/__resource.lua
+++ resources/runcode/__resource.lua
@@ -2,7 +2,16 @@ client_script 'runcode_cl.lua'
 server_script 'runcode_sv.lua'
 server_script 'runcode_web.lua'
 
-client_script 'runcode_shared.lua'
-server_script 'runcode_shared.lua'
+shared_script 'runcode_shared.lua'
+
+shared_script 'runcode.js'
 
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_script 'runcode_ui.lua'
+
+ui_page 'web/nui.html'
+
+files {
+    'web/nui.html'
+}
