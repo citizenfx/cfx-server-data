63b3a44b7098873a1a608c0a54af8d65064270cf
Illinux <snellemany@gmail.com>
2020-03-20 03:03:36 +0100

    removes resource.lua and adds fxmanifest with correct versioning.

---
+++ resources/[test]/example-loadscreen/fxmanifest.lua
@@ -0,0 +1,14 @@
+description 'Example loading screen'
+
+files {
+    'index.html',
+    'keks.css',
+    'bankgothic.ttf',
+    'loadscreen.jpg'
+}
+
+loadscreen 'index.html'
+
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
