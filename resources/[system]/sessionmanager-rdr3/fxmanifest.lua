86cf3e9652aeb4625629f32556d7fb1e5ea53628
Illinux <snellemany@gmail.com>
2020-03-20 03:06:30 +0100

    removes resource.lua and adds fxmanifest with correct versioning.

--- resources/[system]/sessionmanager-rdr3/fxmanifest.lua
+++ resources/[system]/sessionmanager-rdr3/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'bodacious'
 game 'common'
 
 dependencies {
--- resources/[system]/sessionmanager/fxmanifest.lua
+++ resources/[system]/sessionmanager/fxmanifest.lua
@@ -1,4 +1,5 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'bodacious'
+game 'common'
 
 server_script 'server/host_lock.lua'
 client_script 'client/empty.lua'
