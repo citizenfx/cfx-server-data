adfdb89895a905e12ae68d8bdd7a466c8352dad7
astatine <astatine@fivem.net>
2020-04-13 07:45:09 +0200

    chat: register playerJoining

--- resources/[gameplay]/chat/sv_chat.lua
+++ resources/[gameplay]/chat/sv_chat.lua
@@ -7,6 +7,9 @@ RegisterServerEvent('_chat:messageEntered')
 RegisterServerEvent('chat:clear')
 RegisterServerEvent('__cfx_internal:commandFallback')
 
+-- this is a built-in event, but somehow needs to be registered
+RegisterNetEvent('playerJoining')
+
 exports('addMessage', function(target, message)
     if not message then
         message = target
