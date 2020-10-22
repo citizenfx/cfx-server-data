96a3bd1f1dd2db058cdf113432ad777d7c53f4e8
Duck Hunter <duckhunter392@gmail.com>
2020-10-21 20:16:16 -0700

    Updated legacy Player Loop to modern practice

--- resources/[system]/baseevents/deathevents.lua
+++ resources/[system]/baseevents/deathevents.lua
@@ -66,8 +66,8 @@ Citizen.CreateThread(function()
 end)
 
 function GetPlayerByEntityID(id)
-	for i=0,32 do
-		if(NetworkIsPlayerActive(i) and GetPlayerPed(i) == id) then return i end
+	for _,serverId in ipairs(GetActivePlayers()) do
+		if GetPlayerPed(serverId) == id then return serverId end
 	end
 	return nil
 end
