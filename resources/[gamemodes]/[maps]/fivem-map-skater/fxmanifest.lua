5f929c503e8861e473a3f1f62d68c9cf2649964d
Andre Tabarra <tabarra@vexguard.net>
2020-11-04 13:27:03 -0300

    gamemode resources: added fxmanifest meta tags

--- resources/[gamemodes]/[maps]/fivem-map-skater/fxmanifest.lua
+++ resources/[gamemodes]/[maps]/fivem-map-skater/fxmanifest.lua
@@ -1,3 +1,11 @@
+-- This resource is part of the default FiveM/RedM resource pack (aka cfx-server-data)
+-- Modifying or rewriting this resource for local use only is strongly discouraged.
+
+version '1.0.0'
+author 'The CitizenFX Collective'
+description 'FiveM skater map spawnpoints.'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
 resource_type 'map' { gameTypes = { ['basic-gamemode'] = true } }
 
 map 'map.lua'
