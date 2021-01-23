efdc4b2bcdff46f9b2a0acc7dc50409518d07771
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-01-23 08:42:28 +0300

    Update resources/[system]/runcode/fxmanifest.lua
    i forgot a "," thanks to ize3 for finding it
    
    Co-authored-by: ジャック <zeemah@protonmail.ch>

--- resources/[system]/runcode/fxmanifest.lua
+++ resources/[system]/runcode/fxmanifest.lua
@@ -1,5 +1,5 @@
 fx_version 'cerulean'
-games { 'rdr3''gta5' }
+games { 'rdr3', 'gta5' }
 
 author 'Cfx.re <root@cfx.re>'
 description 'Allows server owners to execute arbitrary server-side or client-side JavaScript/Lua code. *Consider only using this on development servers.'
