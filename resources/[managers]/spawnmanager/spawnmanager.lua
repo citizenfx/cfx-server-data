2c6648b1d6c9418d429338b2e84544aba21cbc99
Ryan Bucshon <busheezy@gmail.com>
2021-03-18 16:17:03 -0500

    prevent errors when passing a spawn table rather an index

--- resources/[managers]/spawnmanager/spawnmanager.lua
+++ resources/[managers]/spawnmanager/spawnmanager.lua
@@ -222,6 +222,13 @@ function spawnPlayer(spawnIdx, cb)
 
         if type(spawnIdx) == 'table' then
             spawn = spawnIdx
+
+            -- prevent errors when passing spawn table
+            spawn.x = spawn.x + 0.0001
+            spawn.y = spawn.y + 0.0001
+            spawn.z = spawn.z + 0.0001
+
+            spawn.heading = spawn.heading and (spawn.heading + 0.01) or 0
         else
             spawn = spawnPoints[spawnIdx]
         end
