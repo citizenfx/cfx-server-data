7c62831c969ca5d541a099654bec1b4126ab2c28
Christopher Nelson <chrisnelson@bssecure.net>
2021-10-19 23:59:31 +0100

    feat(chat): Allow 'togglechat' for RDR

--- resources/[gameplay]/chat/cl_chat.lua
+++ resources/[gameplay]/chat/cl_chat.lua
@@ -232,21 +232,21 @@ if not isRDR then
   if RegisterKeyMapping then
     RegisterKeyMapping('toggleChat', 'Toggle chat', 'keyboard', 'l')
   end
-
-  RegisterCommand('toggleChat', function()
-    if chatHideState == CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE then
-      chatHideState = CHAT_HIDE_STATES.ALWAYS_SHOW
-    elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_SHOW then
-      chatHideState = CHAT_HIDE_STATES.ALWAYS_HIDE
-    elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_HIDE then
-      chatHideState = CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE
-    end
-
-    isFirstHide = false
-
-    SetResourceKvp('hideState', tostring(chatHideState))
-  end, false)
 end
+
+RegisterCommand('toggleChat', function()
+  if chatHideState == CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE then
+    chatHideState = CHAT_HIDE_STATES.ALWAYS_SHOW
+  elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_SHOW then
+    chatHideState = CHAT_HIDE_STATES.ALWAYS_HIDE
+  elseif chatHideState == CHAT_HIDE_STATES.ALWAYS_HIDE then
+    chatHideState = CHAT_HIDE_STATES.SHOW_WHEN_ACTIVE
+  end
+
+  isFirstHide = false
+
+  SetResourceKvp('hideState', tostring(chatHideState))
+end, false)
 
 Citizen.CreateThread(function()
   SetTextChatEnabled(false)
