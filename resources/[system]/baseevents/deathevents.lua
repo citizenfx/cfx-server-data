d53d199e2e5375b96c125c82993f0c12b302982f
Idris Dev <IdrisDose@users.noreply.github.com>
2022-03-29 15:31:30 +1100

    fix (baseevents/deathevents): use GetConvarInt instead of hardcoded 32 for GetPlayerByEntityID(id).

--- resources/[system]/baseevents/deathevents.lua
+++ resources/[system]/baseevents/deathevents.lua
@@ -66,8 +66,8 @@ Citizen.CreateThread(function()
 end)
 
 function GetPlayerByEntityID(id)
-	for i=0,32 do
-		if(NetworkIsPlayerActive(i) and GetPlayerPed(i) == id) then return i end
-	end
-	return nil
+    for i=0,GetConvarInt('sv_maxclients', 32) do
+        if(NetworkIsPlayerActive(i) and GetPlayerPed(i) == id) then return i end
+    end
+    return nil
 end
